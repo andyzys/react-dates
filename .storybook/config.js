f2a566f3dbd5dada2227092639cc5bc259c7ce4f
zouyongsheng <zouyongsheng@youzan.com>
2020-01-01 15:49:41 +0800

    feat: 删除部分示例

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,4 @@
 import React from 'react';
-
-if (process.env.NODE_ENV !== 'production') {
-  const whyDidYouRender = require('@welldone-software/why-did-you-render');
-  whyDidYouRender(React);
-}
-
 import moment from 'moment';
 import aphroditeInterface from 'react-with-styles-interface-aphrodite';
 
@@ -18,21 +12,10 @@ import '../css/storybook.scss';
 
 registerInterfaceWithDefaultTheme(aphroditeInterface);
 
-addDecorator((story) => {
+addDecorator(story => {
   moment.locale('en');
   return story();
 });
-
-function getLink(href, text) {
-  return `<a href=${href} rel="noopener noreferrer" target="_blank">${text}</a>`;
-}
-
-const README = getLink('https://github.com/airbnb/react-dates/blob/master/README.md', 'README');
-const wrapperSource = getLink('https://github.com/airbnb/react-dates/tree/master/examples', 'wrapper source');
-
-const helperText = `All examples are built using a wrapper component that is not exported by
-  react-dates. Please see the ${README} for more information about minimal setup or explore
-  the ${wrapperSource} to see how to integrate react-dates into your own app.`;
 
 addDecorator(story => (
   <div>
@@ -47,12 +30,9 @@ addDecorator(story => (
         padding: '8px 40px 8px 8px',
         overflow: 'scroll',
       }}
-      dangerouslySetInnerHTML={{ __html: helperText }}
     />
 
-    <div style={{ marginTop: 7 * 8 }}>
-      {story()}
-    </div>
+    <div style={{ marginTop: 7 * 8 }}>{story()}</div>
   </div>
 ));
 
@@ -63,17 +43,6 @@ setOptions({
 
 function loadStories() {
   require('../stories/DateRangePicker');
-  require('../stories/DateRangePicker_input');
-  require('../stories/DateRangePicker_calendar');
-  require('../stories/DateRangePicker_day');
-  require('../stories/SingleDatePicker');
-  require('../stories/SingleDatePicker_input');
-  require('../stories/SingleDatePicker_calendar');
-  require('../stories/SingleDatePicker_day');
-  require('../stories/DayPickerRangeController');
-  require('../stories/DayPickerSingleDateController');
-  require('../stories/DayPicker');
-  require('../stories/PresetDateRangePicker');
 }
 
 setAddon(infoAddon);
